84953b46dc585965856f37a597aa414df9c82d04
mcerny2626 <mcerny2626@gmail.com>
2026-03-18 14:22:12 -0400

    Changed death message

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -3,7 +3,7 @@ extends Area2D
 @onready var timer: Timer = $Timer
 
 func _on_body_entered(body: Node2D) -> void:
-	print("You Died lmao")
+	print("You Died")
 	timer.start()
 
 func _on_timer_timeout() -> void:
